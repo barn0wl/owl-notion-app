948baaac8044e6d3f7a287bbeb2d1db75127d600
barn0wl <56521094+barn0wl@users.noreply.github.com>
2024-03-09 17:42:58 +0000

    fixed port

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const fastify = require('fastify')({
   })
   
   // Run the server!
-  fastify.listen({ port: 3000 }, function (err, address) {
+  fastify.listen({ port: process.env.PORT, host: "0.0.0.0" }, function (err, address) {
     if (err) {
       fastify.log.error(err)
       process.exit(1)
     }
-    // Server is now listening on ${address}
+    console.log(`Server is now listening on ${address}`)
   })
