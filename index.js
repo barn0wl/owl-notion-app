48b6a02b61aabee1e878d6f48f5bbfdee6dc227b
barn0wl <56521094+barn0wl@users.noreply.github.com>
2024-03-09 16:38:16 +0000

    changes to make app work daily and add tasks properly

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 
 const { Client } = require('@notionhq/client')
-const cron = require('node-cron');
+const later = require('@breejs/later');
+const cronstrue = require('cronstrue');
+later.date.localTime();
 
 const notion = new Client ({auth: process.env.NOTION_KEY})
+const databaseId = process.env.NOTION_DATABASE_ID;
 
-const recurringTasks = []
+const uniqueRecurrTasks = []
 
-async function getRecurringTasks() {
-    const databaseId = process.env.NOTION_DATABASE_ID;
+async function getUniqueRecurr() {
     const response = await notion.databases.query({
         database_id: databaseId,
         filter: {
 
-            //make sure that the task is recurring, not archived, and also isn't already in our array
+            //make sure that the task is recurring, not archived
             "and": [ {
                 "property": "Recurring",
                 "checkbox": {
@@ -29,58 +31,145 @@ async function getRecurringTasks() {
             ]
         }
     })
+    // here, we make sure that the same task isnt repeated (uniquetask)
     for (let task of response.results) {
-        recurringTasks.push(task)
+        const uniqueTask = !uniqueRecurrTasks.some(t => t.properties.Name.title[0].text.content === task.properties.Name.title[0].text.content)
+        if (uniqueTask) {
+            console.log('this task isnt already in the list')
+            uniqueRecurrTasks.push(task)
+        }
+        else {
+            console.log('this task is already in the list')
+        }
     }
 }
 
 
-async function createRecurringTasks() {
-    const date = new Date();
-    const databaseId = process.env.NOTION_DATABASE_ID;
-    for(let task of recurringTasks) {
-        console.log(`creating ${task.properties.Name.title[0].text.content} task`)
-        const response = await notion.pages.create({
-            "parent": {
-                "type": "database_id",
-                "database_id": databaseId
-            },
-            "properties": {
-                "Name": {
-                    "title": [
-                        {
-                            "text": {
-                                "content": task.properties.Name.title[0].text.content
-                            }
-                        }
-                    ]
-                },
-                "Recurring": {
-                    "checkbox": true
-                },
-                "Planned": {
-                    "date": {
-                        "start": date
-                    }
-                },
-                "Cron": {
-                    "rich_text": [
-                        {
-                            "text":  {
-                                "content": task.properties.Cron.rich_text[0].text.content
-                            }
-                        }
-                    ]
+async function createNextRecurr() {
+    for(let rtask of uniqueRecurrTasks) {
+        var sched = later.parse.cron(rtask.properties.Cron.rich_text[0].text.content);
+        const nextDate = later.schedule(sched).next(1);
+        //here, we look inside the database to see if the next occurence of current task isnt already created
+        const response1 = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+
+                "and": [ {
+                    "property": "Recurring",
+                    "checkbox": {
+                    "equals": true
                 }
+                },
+                {
+                    "property": "Archive",
+                    "checkbox": {
+                    "equals": false
+                }
+                },
+                {
+                    "property": "Cron",
+                    "rich_text": {
+                        "equals": rtask.properties.Cron.rich_text[0].text.content
+                      }
+                },
+                {
+                    "property": "Planned",
+                    "date": {
+                        "equals": nextDate
+                    }
+                }
+                ]
             }
         })
-        console.log(response)
+        //we only create a new iteration of the task if it isnt already there
+        const recurrTaskNames = response1.results.map(t => t.properties.Name.title[0].text.content)
+        if (!recurrTaskNames.includes(rtask.properties.Name.title[0].text.content)) {
+            console.log('the next iteration of this task hasnt been created yet')
+            console.log(`creating ${rtask.properties.Name.title[0].text.content} task`)
+            console.log(`next date for this task: ${nextDate}`)
+            const response2 = await notion.pages.create({
+                "parent": {
+                    "type": "database_id",
+                    "database_id": databaseId
+                },
+                "properties": {
+                    "Name": {
+                        "title": [
+                            {
+                                "text": {
+                                    "content": rtask.properties.Name.title[0].text.content
+                                }
+                            }
+                        ]
+                    },
+                    "Recurring": {
+                        "checkbox": true
+                    },
+                    "Planned": {
+                        "date": {
+                            "start": nextDate
+                        }
+                    },
+                    "Cron": {
+                        "rich_text": [
+                            {
+                                "text":  {
+                                    "content": rtask.properties.Cron.rich_text[0].text.content
+                                }
+                            }
+                        ]
+                    },
+                    "Interval": {
+                        "rich_text": [
+                            {
+                                "text":  {
+                                    "content": cronstrue.toString(rtask.properties.Cron.rich_text[0].text.content)
+                                }
+                            }
+                        ]
+                    }
+                }
+            })
+            console.log(response2)
+        }
+        else {
+            console.log('the next iteration of this task has already been created')
+        }
     }
 }
 
-getRecurringTasks().then((result) => {
-    createRecurringTasks()
-}).catch((err) => {
+async function updatingTasks() {
+    await getUniqueRecurr();
+    await createNextRecurr();
+}
 
-});
+
+function executeFunctionAtSpecificTime(hour, minute, second, func) {
+    const now = new Date();
+    let millisUntilSpecifiedTime = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      hour,
+      minute,
+      second,
+      0
+    ) - now;
+  
+    // Ensure the specified time is in the future
+    if (millisUntilSpecifiedTime < 0) {
+      // If the specified time has already passed today,
+      // schedule it for the same time tomorrow
+      millisUntilSpecifiedTime += 24 * 60 * 60 * 1000;
+    }
+  
+    setTimeout(function () {
+      func();
+      // Schedule the function to run again at the same time tomorrow
+      executeFunctionAtSpecificTime(hour, minute, second, func);
+    }, millisUntilSpecifiedTime);
+  }
+  
+executeFunctionAtSpecificTime(0, 0, 0, updatingTasks);
+  
 
